379ebae44827dbd464de8d7759c4339a52e88656
aRinak <grisina047@gmail.com>
2022-10-06 22:39:31 +0300

    v3

---
+++ src/test/java/TestPost.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.urlEncodingEnabled;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+
+public class TestPost {
+
+    @Test
+    void testPost() {
+
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+                .body("json", equalTo(null))
+                .body("url", equalTo("https://postman-echo.com/post"))
+        ;
+    }
+}
